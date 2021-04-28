c2e36611fa04d15f7703807ca465d88da689c5a1
Brent <brentbaughan1@gmail.com>
2021-04-27 20:35:49 -0400

    added notes router

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
-const express = require('express');
+const express = require("express");
 
 const PORT = process.env.PORT || 4040;
 
-const app = express()
+const app = express();
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json())
+app.use(express.json());
 
-app.use(express.static('./public'));
+app.use(express.static("./public"));
 
-app.get('/', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
+app.get("/", (req, res) => res.sendFile(`${__dirname}/public/index.html`));
+
+app.get("/notes", (req, res) => res.sendFile(`${__dirname}/public/notes.html`));
 
 app.listen(PORT, () => {
-    console.log(`App is currently running on port ${PORT}`)
+  console.log(`App is currently running on port ${PORT}`);
 });
