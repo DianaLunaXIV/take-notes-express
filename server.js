b2edd1660d76db023480cf735dbe5fc343960fc9
Brent B <brentbaughan1@gmail.com>
2021-04-27 22:39:43 -0400

    GET /api/notes returns JSON string, containing some extraneous newline escapes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const fs = require("fs");
+//import { v4 as uuidv4 } from 'uuid';
 const PORT = process.env.PORT || 4040;
 
 const app = express();
@@ -13,10 +14,18 @@ app.get("/", (req, res) => res.sendFile(`${__dirname}/public/index.html`));
 
 app.get("/notes", (req, res) => res.sendFile(`${__dirname}/public/notes.html`));
 
-app.get("*", (req, res) => res.sendFile(`${__dirname}/public/index.html`));
-
 app.get("/api/notes", (req, res) => {
   //TODO: read from db.json and return all notes as JSON
+  fs.readFile(`${__dirname}/db/db.json`, 'utf8', (err, data) => {
+      if (err) throw err
+      res.json(data)
+  })
+});
+
+app.get("*", (req, res) => res.sendFile(`${__dirname}/public/index.html`));
+
+app.post("/api/notes", (req, res) => {
+    //TODO: use uuidv4 to generate unique id and push note object to db.json
 });
 
 app.listen(PORT, () => {
