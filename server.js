e2147a763597fa4632134f165ca2083c6c181b74
Brent <brentbaughan1@gmail.com>
2021-04-27 20:48:08 -0400

    added catch route for undefined routes

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ app.get("/", (req, res) => res.sendFile(`${__dirname}/public/index.html`));
 
 app.get("/notes", (req, res) => res.sendFile(`${__dirname}/public/notes.html`));
 
+app.get("*", (req, res) => res.sendFile(`${__dirname}/public/index.html`));
+
 app.listen(PORT, () => {
   console.log(`App is currently running on port ${PORT}`);
 });
